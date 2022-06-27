661e0306af23447d3b82141614c90f10094606a2
Kermola <blumming@hotmail.com>
2022-06-27 21:31:57 +0100

    Create computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello!")
+function computerPlay() {
+    const computerSelectionArray = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
+    return computerSelectionArray[Math.floor(Math.random()*computerSelectionArray.length)];
+    console.log(computerSelectionArray[1]);
+}
+
+computerPlay();
