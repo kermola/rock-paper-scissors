eb1bbb326e65f7713778b4038542f71cf2816ed5
Kermola <blumming@hotmail.com>
2022-06-27 22:08:37 +0100

    Add playRound function with basic win results.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Function to randomly return "Rock", "Paper", or "Scissors"
 function computerPlay() {
     const computerSelectionArray = [
         "rock",
@@ -5,7 +6,22 @@ function computerPlay() {
         "scissors"
     ];
     return computerSelectionArray[Math.floor(Math.random()*computerSelectionArray.length)];
-    console.log(computerSelectionArray[1]);
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    console.log("Computer chose " + computerSelection);
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return ("Draw! You both chose the same thing");
+    }
+    else if ((playerSelection == "rock" && computerSelection =="scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper")) {
+        const playerSelectionUpperCased = playerSelection.replace(/(^\w)/g, m => m.toUpperCase());
+        const computerSelectionUpperCased = computerSelection.replace(/(^\w)/g, m => m.toUpperCase());
+        return ("You win! " + playerSelectionUpperCased + " beats " + computerSelectionUpperCased + ".");
+        }
+    else {
+        return ("Not yet coded!");
+    }
+}
