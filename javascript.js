0d4069e4c6a8fa13751920c15da72101118126ad
Kermola <blumming@hotmail.com>
2022-06-28 00:46:14 +0100

    Add 5 round game function and round scoring.

--- javascript.js
+++ javascript.js
@@ -8,22 +8,62 @@ function computerPlay() {
     return computerSelectionArray[Math.floor(Math.random()*computerSelectionArray.length)];
 }
 
+function playerWinRound() {
+    playerRoundWins++;
+}
+
+function computerWinRound() {
+    computerRoundWins++;
+}
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    const playerSelectionUpperCased = playerSelection.replace(/(^\w)/g, m => m.toUpperCase());
-    const computerSelectionUpperCased = computerSelection.replace(/(^\w)/g, m => m.toUpperCase());
+    playerSelectionUpperCased = playerSelection.replace(/(^\w)/g, m => m.toUpperCase());
+    computerSelectionUpperCased = computerSelection.replace(/(^\w)/g, m => m.toUpperCase());
     console.log("Computer chose " + computerSelectionUpperCased);
-    if (playerSelection == computerSelection) {
-        return ("Draw! You both chose the same thing");
-    }
-    else if ((playerSelection == "rock" && computerSelection =="scissors") ||
-            (playerSelection == "paper" && computerSelection == "rock") ||
-            (playerSelection == "scissors" && computerSelection == "paper")) {
-        return ("You win! " + playerSelectionUpperCased + " beats " + computerSelectionUpperCased + ".");
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+        if (playerSelection == computerSelection) {
+            return ("Draw! You both chose the same thing");
         }
+        else if ((playerSelection == "rock" && computerSelection =="scissors") ||
+                (playerSelection == "paper" && computerSelection == "rock") ||
+                (playerSelection == "scissors" && computerSelection == "paper")) {
+                playerWinRound();
+                return ("You win! " + playerSelectionUpperCased + " beats " + computerSelectionUpperCased + ".");
+            }
+        else {
+            computerWinRound();
+            return ("You lose! " + computerSelectionUpperCased + " beats " + playerSelectionUpperCased);
+        }
+    }
     else {
-        return ("You lose! " + computerSelectionUpperCased + " beats " + playerSelectionUpperCased);
+        computerWinRound();
+        return ("Error: Gesture not recognized, you lose this round.");
     }
 }
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    playerRoundWins = 0;
+    computerRoundWins = 0;
+    for (let i = 0; i < 5; i++) {
+        // let computerSelection = computerPlay();
+        let gesture = prompt("What gesture will you play?");
+        console.log("Round " + i + ":");
+        console.log(playRound(gesture, computerPlay()));
+    }
+    if (playerRoundWins > computerRoundWins) {
+        return ("You win the game, " + playerRoundWins + " rounds to " + computerRoundWins);
+    }
+    else if (playerRoundWins < computerRoundWins) {
+        return ("You lose the game, " + playerRoundWins + " rounds to " + computerRoundWins);
+    }
+    else {
+        return ("It's a tie!  You both won " + playerRoundWins + " rounds.");
+    }
+}
+
